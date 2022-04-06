5015669d903f5922b16e2bd5ac03486b9d2a90de
David Amos <davidamos@cygnet.it>
2022-04-06 12:37:12 +0100

    Commented out SDL functions. Added javidx9 console rendering. Added #defines to eliminate some magic numbers

--- chip8.cpp
+++ chip8.cpp
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define GFXARRAY 2048
+#define STACK_SIZE 16
+#define REGISTERS 16
+#define MEM_SIZE 4096
+#define FONTSET_SIZE 80
+#define ROMSTART_OFFSET 512
 /*This array defines the CHIP-8's fontset. Recall that the system draws entire sprites to pixels on the screen in a given location
  * and you may realize what this array is doing. Each character is 4 pixels across, and 5 pixels down. Look at the very first entry,
  * 0xF0, and recall that in binary, 0xF0 is 1111 0000. The CHIP-8 ignores the last 4 bits, so when it sees 1111, it knows to fill the first
@@ -46,42 +52,42 @@ void emu::initialize_chip8() {
 	opcode = 0;	//Reset the opcode...
 	index = 0;	//...the index register...
 	sp = 0;		//...and the stack pointer
-	
-	for(int i = 0; i < 2048; i++) {
+
+	for(int i = 0; i < GFXARRAY; i++) {
 		graphics[i] = 0;	//Clearing the graphics array. Just make it all 0...there are 2048 items in the array, so we loop that many times
 	}
-	
-	for(int i = 0; i < 16; i++) {
+
+	for(int i = 0; i < STACK_SIZE; i++) {
 		stack[i] = 0;		//Clear the stack. 16 items, 16 loops.
 	}
-	
-	for(int i = 0; i < 16; i++) {
+
+	for(int i = 0; i < REGISTERS; i++) {
 		input[i] = registers[i] = 0;	//Clear the input and cpu registers too!
 	}
-	
-	for(int i = 0; i < 4096; i++) {
+
+	for(int i = 0; i < MEM_SIZE; i++) {
 		mem[i] = 0;			//Cleaning cleaning....
 	}
-	
-	for(int i = 0; i < 80; i++) {
+
+	for(int i = 0; i < FONTSET_SIZE; i++) {
 		mem[i] = fontset[i];	//We'll load the fontset as part of the initialization process. It needs to be here for the system to use!
 	}
-	
+
 	//Resetting system timers
 	delayTimer = 0;
 	soundTimer = 0;
-	
+
 	//And finally, clear the screen! Just the once is fine.
 	drawFlag = true;
-	
+
 	srand(time(NULL));
 }
- 
+
 bool emu::loadRom(const char * fileName) {
 	initialize_chip8(); 					//Picture this as turning the CHIP-8 on, if it helps.
 	printf("Starting %s...\n", fileName); 	// Just a little debug line, pretty self-explanatory
-	
-	FILE * pFile = fopen(fileName, "rb"); 	//Look up fopen if you need more help here, but briefly, fopen returns a stream that can be 
+
+	FILE * pFile = fopen(fileName, "rb"); 	//Look up fopen if you need more help here, but briefly, fopen returns a stream that can be
 											//referred to with pFile. It takes two parameters, the path of the file in question and
 											//the mode. "rb" here means "read and binary mode". That's what we need!
 	if(pFile == NULL) {
@@ -89,9 +95,9 @@ bool emu::loadRom(const char * fileName) {
 													//is a file stream that's part of the stdlib.
 		return false;								//If something screws up loading the rom, we're gonna want to abort the whole thing.
 	}
-	
+
 	//Next up, we're gonna check the size of the rom. We'll need to do this to make sure it will fit in the emulated system's memory!
-	
+
 	fseek(pFile, 0, SEEK_END);	//fseek just moves the file position indicator. Read this as "move the indicator 0 bytes from the end of the
 								//file pointed to by pFILE".
 	long romSize = ftell(pFile);//Since we're at the end of the file, ftell will tell us how many bytes there are from the end of the file
@@ -99,7 +105,7 @@ bool emu::loadRom(const char * fileName) {
 	rewind(pFile);				//Now go back to the beginning of pFile. You rewind VHS tapes when you finish the movie right? :)
 	printf("Rom size: %d\n", (int)romSize); //Self explanatory. Note the type cast for romSize. We want it as a long, but we don't need to
 											//print it as one, right?
-	
+
 	char * buffer = (char*)malloc(sizeof(char) * romSize);	//malloc returns a void* pointer to the beginning of a block of memory. We'll
 															//be needing to dereference this block of memory later (in order to fill the
 															//emulated system memory with the same data) so what we want as a char* pointer.
@@ -110,35 +116,35 @@ bool emu::loadRom(const char * fileName) {
 		fputs("Memory error!", stderr);						//Quick sanity check. If malloc failed to make a buffer for us...
 		return false;										//We want to abort the rest of what we're doing.
 	}
-	
+
 	/*So we've loaded the file, we've recorded it's size, and we've allocated memory for it. Next up is to copy it to the emulated system's
 	 * memory*/
-	 
+
 	size_t result = fread(buffer, 1, romSize, pFile);		//fread takes, in this order, a pointer to a block of memory to write to, the size
 															//in bytes for each read, the number of reads to perform, and the file stream to
 															//read from. fread returns the number of successfully read elements.
-	
+
 	if(result != romSize) { 								//In our code, result and romSize should be the same size. If they're not, something's wrong.
 		fputs("Reading error", stderr);
 		return false;										//So abort!
 	}
-	
-	if((4096-512) > romSize) {								//ROM goes into system memory from location 0x200 (512). We don't want to do
+
+	if((MEM_SIZE-ROMSTART_OFFSET) > romSize) {								//ROM goes into system memory from location 0x200 (512). We don't want to do
 		for(int i = 0; i < romSize; i++) {
-			mem[i+512] = buffer[i];
+			mem[i+ROMSTART_OFFSET] = buffer[i];
 		}
 	}
 	else {
 		printf("ROM too large for memory!");
 	}
-	
+
 	//Okay, we're done with the file now. It should be in program memory and emulated system memory at this point. Let's clean up
 	//and close the file and free the buffer. And return true at the end, since everything should've been successful
 	fclose(pFile);
 	free(buffer);
-	
+
 	return true;
-	
+
 }
 
 //Moment of truth. The system is initialized. The file is loaded and in the emulated system's memory. Now we fetch, decode, and execute.
@@ -147,7 +153,7 @@ bool emu::loadRom(const char * fileName) {
 void emu::emuCycle()
 {
 	//Remember the first step? Fetching the opcode!
-	
+
 	opcode = mem[pc] << 8 | mem[pc+1]; 	//What happened here? Recall that an opcode is two bytes long, but each element of our memory array
 										//is only one byte long. We need both bytes to know what the opcode is. So we get the first byte at
 										//location indicated by our program counter (pc), and we shift the bits 8 bits to the left. Meaning,
@@ -156,17 +162,17 @@ void emu::emuCycle()
 										//total, 1111 1111 1010 0010 stored in our opcode variable. Remember, OR means if either bit is SET,
 										//the result is a SET bit. Look at the last 8 bits of both examples. See how it works now? Look up
 										//bitwise OR for more help later.
-										
+
 	/*In any event, we've fetched our opcode now. Now we have to decode it. The CHIP-8 has 35 opcodes. You can find the opcode table
-	 * at https://en.wikipedia.org/wiki/CHIP-8#Opcode_table. Once we have the opcode, we need to know which specific opcode it is in 
+	 * at https://en.wikipedia.org/wiki/CHIP-8#Opcode_table. Once we have the opcode, we need to know which specific opcode it is in
 	 * order to know what we should do with it. The way we do this is by using bitwise AND. Take the first instruction in the table
 	 * for instance. 0x0NNN means we should execute the RCA 1802 program at address NNN. So if our opcode is 0x0FFF, the first four bits are
 	 * 0b0000. If we AND 0x0FFF against 0xF000, we'll end up with 0x0000, meaning the first four bits are 0b000. Look at the table again and
-	 * you'll notice that 3 instructions will fit that criteria. Not a problem, we'll just do another switch test that will identify any one 
+	 * you'll notice that 3 instructions will fit that criteria. Not a problem, we'll just do another switch test that will identify any one
 	 * of these 3 instructions. Another example? ANNN sets the index register to the address NNN. 0xANNN AND 0xF000 will give you 0xA000, so
 	 * now you know which instruction it is! Since this is an interpreter, this is basically going to be our pattern. Test for a certain case
 	 * and, if it's satisfied, execute that opcode. Fetch, decode, execute!*/
-	 
+
 	switch(opcode & 0xF000)
 	{
 		case(0x0000):													//Three commands satisfy this test, so we need more tests.
@@ -176,29 +182,29 @@ void emu::emuCycle()
 					//SCREEN CLEAR!
 					pc+= 2;
 					break;
-				
+
 				case(0x00EE):
 					//RETURN FROM SUBROUTINE
 					pc = stack[--sp];
-					pc += 2;					
+					pc += 2;
 					break;
-					
+
 				default:
-					printf("RUN MACHINE CODE AT ADDRESS 0x0NNN (DEPRECATED\n");				
+					printf("RUN MACHINE CODE AT ADDRESS 0x0NNN (DEPRECATED\n");
 			}
 		break;
-		
+
 		case(0x1000):
 			//JUMP TO SUBROUTINE AT ADDRESS 0x1NNN
 			pc = opcode & 0x0FFF;
 			break;
-		
+
 		case(0x2000):
 			//CALL SUBROUTINE AT ADDRESS 0x2NNN
 			stack[sp++] = pc;
-			pc = opcode & 0x0FFF;			
+			pc = opcode & 0x0FFF;
 			break;
-		
+
 		case(0x3000):
 			//SKIP THE NEXT INSTRUCTION IF VX EQUALS NN (0x3XNN)
 			if(registers[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) {
@@ -207,7 +213,7 @@ void emu::emuCycle()
 				pc += 2;
 			}
 			break;
-		
+
 		case(0x4000):
 			//SKIP THE NEXT INSTRUCTION IF VX DOES NOT EQUAL NN (0x4XNN)
 			if(registers[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
@@ -216,7 +222,7 @@ void emu::emuCycle()
 				pc += 2;
 			}
 			break;
-		
+
 		case(0x5000):
 			//SKIP THE NEXT INSTRUCTION IF VX = VY (0x5XY0)
 			if(registers[(opcode & 0x0F00) >> 8] == registers[(opcode & 0x00F0) >> 4]){
@@ -225,19 +231,19 @@ void emu::emuCycle()
 				pc += 2;
 			}
 			break;
-		
+
 		case(0x6000):
 			//SET VX TO NN (0x6XNN)
 			registers[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF);
 			pc += 2;
 			break;
-		
+
 		case(0x7000):
 			//ADD NN TO VX WITHOUT SETTING CARRY FLAG (0x7XNN)
 			registers[(opcode & 0x0F00) >> 8] += (opcode & 0x00FF);
 			pc += 2;
 			break;
-			
+
 		case(0x8000):													//Several instructions satisfy this test, so we need another
 			switch(opcode & 0x000F)										//It's only the last hex digit that changes between them, so this
 			{
@@ -246,25 +252,25 @@ void emu::emuCycle()
 					registers[(opcode & 0x0F00) >> 8] = registers[(opcode & 0x00F0) >> 4];
 					pc += 2;
 					break;
-				
+
 				case(0x0001): //0x8001...and so on
 					//SET VX to VX|=VY (0x8XY1)
-					registers[(opcode & 0x0F00) >> 8] |= registers[(opcode & 0x00F0) >> 4]; 
+					registers[(opcode & 0x0F00) >> 8] |= registers[(opcode & 0x00F0) >> 4];
 					pc += 2;
 					break;
-				
+
 				case(0x0002):
 					//SET VX to VX&=VY
 					registers[(opcode & 0x0F00) >> 8] &= registers[(opcode & 0x00F0) >> 4];
 					pc += 2;
 					break;
-					
+
 				case(0x0003):
 					//SET VX to VX^=VY
 					registers[(opcode & 0x0F00) >> 8] ^= registers[(opcode & 0x00F0) >> 4];
 					pc += 2;
 					break;
-				
+
 				case(0x0004):
 					//ADD VY TO VX (0x8XY4). SET CARRY FLAG IF REQUIRED
 					if(registers[(opcode & 0x00F0) >> 4] > (0xFF - registers[opcode & 0x0F00])){
@@ -273,27 +279,27 @@ void emu::emuCycle()
 						registers[0xF] = 0;
 					}
 					registers[(opcode & 0x0F00) >> 8] += registers[(opcode & 0x00F0) >> 4];
-					pc += 2;					
+					pc += 2;
 					break;
-					
+
 				case(0x0005):
 					//SUBTRACT VY FROM VX. UNSET CARRY FLAG IF REQUIRED
 					if(registers[(opcode & 0x00F0) >> 4] > registers[(opcode & 0x0F00) >> 8]) {
 						registers[0xF] = 0;
 					} else {
 						registers[0xF] = 1;
-					}					
+					}
 					registers[(opcode & 0x0F00) >> 8] -= registers[(opcode & 0x00F0)];
 					pc += 2;
 					break;
-				
+
 				case(0x0006):
 					//STORE LEAST SIGNIFICANT BIT OF VX IN VF, THEN SHIFT VX >> 1
 					registers[0xF] = registers[(opcode & 0x0F00) >> 8] & 0x1;
 					registers[(opcode & 0x0F00) >> 8] >>= 1;
 					pc+=2;
 					break;
-				
+
 				case(0x0007):
 					//SUBTRACT VX FROM VY AND STORE RESULT IN VX. VF CLEARED IF THERE'S A BORROW, SET IF NOT
 					if(registers[(opcode & 0x0F00) >> 8] > registers[(opcode & 0x00F0) >> 4]) {
@@ -301,23 +307,23 @@ void emu::emuCycle()
 					} else {
 						registers[0xF] = 1;
 					}
-					
+
 					registers[(opcode & 0x0F00) >> 8] = registers[(opcode & 0x00F0) >> 4] - registers[(opcode & 0x0F00) >> 4];
 					pc += 2;
 					break;
-					
+
 				case(0x000E):
 					//STORE THE MOST SIGNIFICANT BIT OF VX in VF, THEN SHIFT VX << 1
 					registers[0xF] = registers[(opcode & 0x0F00) >> 8] >> 7;
 					registers[(opcode & 0x0F00) >> 8] <<= 1;
 					pc += 2;
 					break;
-					
+
 				default:
 					printf("UNIMPLEMENTED OPCODE: %0.4X\n", opcode);
 				}
 				break;
-					
+
 		case(0x9000):
 			//SKIPS NEXT INSTRUCTION IF VX != VY
 			if(registers[(opcode & 0x0F00) >> 8] != registers[(opcode & 0x00F0) >> 4]) {
@@ -326,24 +332,24 @@ void emu::emuCycle()
 				pc += 2;
 			}
 			break;
-			
+
 		case(0xA000):
 			//SETS I TO ADDRESS NNN
 			index = opcode & 0x0FFF;
 			pc += 2;
 			break;
-			
+
 		case(0xB000):
 			//JUMP TO THE ADDRESS NNN PLUS V0
 			pc = (opcode & 0x0FFF) + registers[0];
 			break;
-			
+
 		case(0xC000):
 			//SET VX to "rand() & NN"
 			registers[(opcode & 0x0F00) >> 8] = (rand()%0xFF) & (opcode & 0x00FF);
 			pc += 2;
 			break;
-			
+
 		case(0xD000):
 			//DXYN. DRAW INSTRUCTIONS. A DOOZY. DO IT LATER.
 			{
@@ -351,7 +357,7 @@ void emu::emuCycle()
 				unsigned short y = registers[(opcode & 0x00F0) >> 4];
 				unsigned short height = opcode & 0x000F;
 				unsigned short pixel;
-				
+
 				registers[0xF] = 0;
 				for(int yline = 0; yline < height; yline++)
 				{
@@ -372,7 +378,7 @@ void emu::emuCycle()
 				drawFlag = true;
 				pc += 2;
 				break;
-			
+
 		case(0xE000):
 			switch(opcode & 0x00FF)
 			{
@@ -401,7 +407,7 @@ void emu::emuCycle()
 					printf("Something broke. Bad.\n");
 				}
 				break;
-					
+
 		case(0xF000):
 			switch(opcode & 0x00FF)
 			{
@@ -410,14 +416,14 @@ void emu::emuCycle()
 					registers[(opcode & 0x0F00) >> 8] = delayTimer;
 					pc += 2;
 					break;
-					
+
 				case(0x000A):
 				{
 					//FX0A WAITING FOR A KEY PRESS TO STORE IN VX
 					//INSTRUCTIONS HALTED UNTIL KEY PRESS, SO DO NOT ADVANCE PC UNTIL KEY PRESS EXISTS
-					
+
 					bool keyPress = false;
-					
+
 					for(int i = 0; i < 16; i++)
 					{
 						if(input[i] != 0)
@@ -426,40 +432,40 @@ void emu::emuCycle()
 							keyPress = true;							//And then flag that key has been pressed.
 						}
 					}
-					
+
 					if(!keyPress)										//If after looping through the entire input array no key has been
 					{													//pressed...
 						return;											//...we need to jump back and try again.
 					}
-					
+
 					pc += 2;											//Only after a successful test can we increment the PC
 					break;
 				}
-					
+
 				case (0x0015):
 					//FX15 SET DELAY TIMER TO VX
 					delayTimer = registers[(opcode & 0x0F00) >> 8];
 					pc += 2;
 					break;
-					
+
 				case(0x0018):
 					//FX18 SET SOUND TIMER TO VX
 					soundTimer = registers[(opcode & 0x0F00) >> 8];
 					pc += 2;
 					break;
-					
+
 				case(0x001E):
 					//FX1E ADD VX TO I
 					index += registers[(opcode & 0x0F00) >> 8];
 					pc += 2;
 					break;
-					
+
 				case(0x0029):
 					//FX29 SET INDEX TO LOCATION OF SPRITE FOR THE CHARACTER IN VX
 					index = registers[(opcode & 0x0F00) >> 8] * 0x5;
 					pc += 2;
 					break;
-					
+
 				case(0x0033):
 					//FX33 STORE THE BCD REPRESENTATION OF VX IN I
 					mem[index]  = registers[(opcode & 0x0F00) >> 8] / 100;
@@ -467,42 +473,42 @@ void emu::emuCycle()
 					mem[index+2]= (registers[(opcode & 0x0F00) >> 8] % 100) % 10;
 					pc += 2;
 					break;
-					
+
 				case(0x0055):
 					//FX55 STORE V0 TO VX IN MEMORY STARTING AT ADDRESS AT INDEX
-					
+
 					for(int i = 0; i < registers[(opcode & 0x0F00) >> 8]; i++)
 					{
 						mem[index+i] = registers[i];
 					}
 					pc += 2;
 					break;
-					
+
 				case(0x0065):
 					//FX65 COPY MEMORY VALUES STARTING AT INDEX INTO REGISTERS V0 THRU VX
-					
+
 					for(int i = 0; i < registers[(opcode & 0x0F00) >> 8]; i++)
 					{
 						registers[i] = mem[index+i];
 					}
 					pc += 2;
 					break;
-					
+
 				default:
 					printf("UNIMPLEMENTED OPCODE: %0.4X\n", opcode);
 				}
 				break;
-					
+
 		default:
 			printf("UNIMPLEMENTED OPCODE: %0.4X\n", opcode);
-		
-	}		
+
+	}
 	//UPDATE TIMERS
 	if(delayTimer > 0)
 	{
 		--delayTimer;
 	}
-	
+
 	if(soundTimer > 0)
 	{
 		if(soundTimer == 1)
@@ -520,16 +526,10 @@ void emu::debugRender()
 	{
 		for(int x = 0; x < 64; ++x)
 		{
-			if(graphics[(y*64) + x] == 0) 
-				printf("O");
-			else 
-				printf(" ");
+			if(graphics[(y*64) + x] == 0)
+				pixel = 0x2588 ;
+			else
+				pixel = ' ';
 		}
-		printf("\n");
 	}
-	printf("\n");
 }
-
-		
-	
-
--- main.cpp
+++ main.cpp
@@ -1,124 +1,141 @@
 #include <stdio.h>
-#include <SDL2/SDL.h>
+#include <Windows.h>
 #include "chip8.h"
 
-//SDL screen constants
+// //SDL screen constants
+//
+// const int SCREEN_HEIGHT = 32;
+// const int SCREEN_WIDTH = 64;
+// const int SCREEN_MULTIPLIER = 1;
+//
+// uint8_t screenBuffer[SCREEN_HEIGHT * SCREEN_WIDTH * 4] = {0};
+// bool bufferDebug = true;
 
-const int SCREEN_HEIGHT = 32;
-const int SCREEN_WIDTH = 64;
-const int SCREEN_MULTIPLIER = 1;
+/**********************
+/Trying to use Javidx9's console rendering techniques...
+/**********************/
 
-uint8_t screenBuffer[SCREEN_HEIGHT * SCREEN_WIDTH * 4] = {0};
-bool bufferDebug = true;
-
+int nScreenWidth = 64;
+int nScreenHeight = 32;
 
 int pitch = 0;
 void* pixels = NULL;
 
 emu chip8;
 
-bool init()
-{
-	if(SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("Could not initialize SDL2! Error: %s\n", SDL_GetError());
-		return false;		
-	}
-}
+// bool init()
+// {
+// 	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+// 	{
+// 		printf("Could not initialize SDL2! Error: %s\n", SDL_GetError());
+// 		return false;
+// 	}
+// }
 
-void close(SDL_Window* window, SDL_Texture* texture, SDL_Renderer* renderer)
-{
-	SDL_DestroyTexture(texture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-}
+// void close(SDL_Window* window, SDL_Texture* texture, SDL_Renderer* renderer)
+// {
+// 	SDL_DestroyTexture(texture);
+// 	SDL_DestroyRenderer(renderer);
+// 	SDL_DestroyWindow(window);
+// 	SDL_Quit();
+// }
+//
+// void copyToBuffer()
+// {
+// 	for(int i = 0; i < 2048; i++)
+// 	{
+// 		if(chip8.graphics[i] == 1)
+// 		{
+// 			screenBuffer[(i*4)] = 255;
+// 			screenBuffer[(i*4)+1] = 255;
+// 			screenBuffer[(i*4)+2] = 255;
+// 			screenBuffer[(i*4)+3] = 255;
+// 		} else {
+// 			screenBuffer[(i*4)] = 0;
+// 			screenBuffer[(i*4)+1] = 0;
+// 			screenBuffer[(i*4)+2] = 0;
+// 			screenBuffer[(i*4)+3] = 255;
+// 		}
+// 	}
+// }
 
-void copyToBuffer()
-{
-	for(int i = 0; i < 2048; i++)
-	{
-		if(chip8.graphics[i] == 1)
-		{
-			screenBuffer[(i*4)] = 255;
-			screenBuffer[(i*4)+1] = 255;
-			screenBuffer[(i*4)+2] = 255;
-			screenBuffer[(i*4)+3] = 255;
-		} else {
-			screenBuffer[(i*4)] = 0;
-			screenBuffer[(i*4)+1] = 0;
-			screenBuffer[(i*4)+2] = 0;
-			screenBuffer[(i*4)+3] = 255;
-		}
-	}
-}
+// bool updateTexture(SDL_Texture* texture)
+// {
+// 	if(SDL_LockTexture(texture, NULL, &pixels, &pitch) != 0)
+// 	{
+// 		printf("Couldn't lock the texture! Error: %s\n", SDL_GetError());
+// 		return false;
+// 	} else
+// 	{
+// 		memcpy(pixels, screenBuffer, pitch * SCREEN_HEIGHT);
+// 	}
+//
+// 	SDL_UnlockTexture(texture);
+// 	return true;
+// }
+//
+// void display(SDL_Renderer* renderer, SDL_Texture* texture)
+// {
+// 	SDL_RenderClear(renderer);
+// 	SDL_RenderCopy(renderer, texture, NULL, NULL);
+// 	SDL_RenderPresent(renderer);
+// }
 
-bool updateTexture(SDL_Texture* texture)
-{
-	if(SDL_LockTexture(texture, NULL, &pixels, &pitch) != 0)
-	{
-		printf("Couldn't lock the texture! Error: %s\n", SDL_GetError());
-		return false;
-	} else
-	{
-		memcpy(pixels, screenBuffer, pitch * SCREEN_HEIGHT);
-	}
-	
-	SDL_UnlockTexture(texture);
-	return true;
-}
-
-void display(SDL_Renderer* renderer, SDL_Texture* texture)
-{
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-	SDL_RenderPresent(renderer);
-}
-
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
 	if(argc < 2)
 	{
 		printf("Usage: emu ROMPATH\n");
 		return 1;
 	}
-	
+
 	if(!chip8.loadRom(argv[1]))
 	{
 		printf("Ya failed.\n");
 		return 1;
 	}
-	init();
-	SDL_Window* window = SDL_CreateWindow("Chip8 by Aerosol", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
-		if (window == NULL)
-		{
-			printf("Window got messed up. Error: %s\n", SDL_GetError());
-			return 1;
-		} 
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-		if(renderer == NULL)
-		{
-			printf("Couldn't create the renderer! Error: %s\n", SDL_GetError());
-			return 1;
-		}
-	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
-		if(texture == NULL)
-		{
-			printf("Couldn't create texture! Error: %s\n", SDL_GetError());
-			return 1;
-		}
-	
-	int quit = 0;
-	SDL_Event e;
-	
-	while(!quit)
+
+	//Create console screen buffer
+
+	wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	// init();
+	// SDL_Window* window = SDL_CreateWindow("Chip8 by Aerosol", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
+	// 	if (window == NULL)
+	// 	{
+	// 		printf("Window got messed up. Error: %s\n", SDL_GetError());
+	// 		return 1;
+	// 	}
+	// SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	// 	if(renderer == NULL)
+	// 	{
+	// 		printf("Couldn't create the renderer! Error: %s\n", SDL_GetError());
+	// 		return 1;
+	// 	}
+	// SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+	// 	if(texture == NULL)
+	// 	{
+	// 		printf("Couldn't create texture! Error: %s\n", SDL_GetError());
+	// 		return 1;
+	// 	}
+
+
+	// SDL_Event e;
+
+	while(1)
 	{
-		while(SDL_PollEvent(&e) != 0)
-		{
-			if(e.type == SDL_QUIT)
-			{
-				quit = 1;
-			} 	
+		short pixel = ' ';
+		screen[nScreenWidth * nScreenHeight - 1] = '\0';
+		WriteConsoleOutputCharacterW(hConsole, screen, nScreenWidth * nScreenHeight, {0, 0}, &dwBytesWritten);
+		// while(SDL_PollEvent(&e) != 0)
+		// {
+		// 	if(e.type == SDL_QUIT)
+		// 	{
+		// 		quit = 1;
+		// 	}
 			/*chip8.graphics[0] = 1;
 			copyToBuffer();
 			if(!bufferDebug)
@@ -130,27 +147,38 @@ int main(int argc, char **argv)
 				bufferDebug = true;
 			}
 			updateTexture(texture);
-			display(renderer, texture);*/	
-			
+			display(renderer, texture);*/
+
+
 			chip8.emuCycle();
-			if(chip8.drawFlag)
+			chip8.debugRender();
+			for(int x = 0; x < nScreenWidth; x++)
 			{
-				copyToBuffer();
-				if(bufferDebug)
-			{
-				for(int i = 0; i < 32; i++)
+				for(int y = 0; y < nScreenHeight; y++)
 				{
-					printf("Location %d: %x\n", i, screenBuffer[i]);
+					if(chip8.graphics[(y*64) + x] == 0)
+						pixel = 0x2588 ;
+					else
+						pixel = ' ';
 				}
-				bufferDebug = false;
 			}
-				updateTexture(texture);
-				display(renderer, texture);
-				chip8.drawFlag = false;
-			}		
-				
-		}
+			// if(chip8.drawFlag)
+			// {
+			// 	copyToBuffer();
+			// 	if(bufferDebug)
+			// {
+			// 	for(int i = 0; i < 32; i++)
+			// 	{
+			// 		printf("Location %d: %x\n", i, screenBuffer[i]);
+			// 	}
+			// 	bufferDebug = false;
+			// }
+			// 	updateTexture(texture);
+			// 	display(renderer, texture);
+			// 	chip8.drawFlag = false;
+			// }
+		// }
 	}
-	close(window, texture, renderer);
+	// close(window, texture, renderer);
 	return 0;
 }
